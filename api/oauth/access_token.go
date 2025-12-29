a6714b3c388f75ed5e202f3a9cfb5ca1026bae1d
Syd Xu <prof.syd.xu@gmail.com>
2025-12-29 17:46:13 +0800

    fix(oauth): AccessToken

--- api/oauth/access_token.go
+++ api/oauth/access_token.go
@@ -8,14 +8,11 @@ import (
 )
 
 // AccessToken 获得长期有效的访问令牌
-func AccessToken(ctx context.Context, clt *core.SDKClient, code string) (*oauth.AccessToken, error) {
-	req := oauth.AccessTokenRequest{
-		AppID:    clt.AppID(),
-		Secret:   clt.Secret(),
-		AuthCode: code,
-	}
+func AccessToken(ctx context.Context, clt *core.SDKClient, req *oauth.AccessTokenRequest) (*oauth.AccessToken, error) {
+	req.AppID = clt.AppID()
+	req.Secret = clt.Secret()
 	var ret oauth.AccessTokenResponse
-	if err := clt.Post(ctx, "v1.3/oauth2/access_token/", &req, &ret, ""); err != nil {
+	if err := clt.Post(ctx, "v1.3/oauth2/access_token/", req, &ret, ""); err != nil {
 		return nil, err
 	}
 	return ret.Data, nil
