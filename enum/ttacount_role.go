9f2ee7e1a2395a6da2cf48a2a842be0a6f1ba433
bububa <prof.syd.xu@gmail.com>
2026-09-08 12:53:58 +0800

    feat(tto/campaign): We have added the enum values PUBLISH_VIDEO_ADS_ONLY, PUBLISH_VIDEO_SHOW_ON_TT_PROFILE_AND_ADS, SHOWCASE, and SERIES to the tt_account_roles parameter across three endpoints: /bc/asset/assign/, /bc/asset/get/, and /bc/asset/admin/get/. This update allows you to assign granular TikTok account permissions, such as 'Only show as ads', 'Show on TikTok profile and as ads', Showcase, and Series, to Business Center members, offering you precise control over member access and asset management.

--- enum/ttacount_role.go
+++ enum/ttacount_role.go
@@ -10,4 +10,12 @@ const (
 	TTAccountRole_LIVE TTAccountRole = "LIVE"
 	// TTAccountRole_DIRECT_MESSAGE：私信权限，包括允许所有人向您发送私信，阅读和管理您收件箱中的私信，以及代表您向其他账号发送消息或进行互动。
 	TTAccountRole_DIRECT_MESSAGE TTAccountRole = "DIRECT_MESSAGE"
+	// TTAccountRole_PUBLISH_VIDEO_ADS_ONLY "Only show as ads" permission. Videos will only get paid traffic and won't be published to your TikTok profile.
+	TTAccountRole_PUBLISH_VIDEO_ADS_ONLY TTAccountRole = "PUBLISH_VIDEO_ADS_ONLY"
+	// TTAccountRole_PUBLISH_VIDEO_SHOW_ON_TT_PROFILE_AND_ADS "Show on TikTok profile and as ads" permission. Videos will get both organic and paid traffic and will be published to your TikTok profile.
+	TTAccountRole_PUBLISH_VIDEO_SHOW_ON_TT_PROFILE_AND_ADS TTAccountRole = "PUBLISH_VIDEO_SHOW_ON_TT_PROFILE_AND_ADS"
+	// TTAccountRole_SHOWCASE Showcase permissions (Only available in some regions), including accessing your own profile information and products from Showcase
+	TTAccountRole_SHOWCASE TTAccountRole = "SHOWCASE"
+	// TTAccountRole_SERIES Series permissions, including accessing your own profile information and TikTok Series
+	TTAccountRole_SERIES TTAccountRole = "SERIES"
 )
